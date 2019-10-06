47f849e8f3a0da3a936406191305717e543a8858
labohkip81 <laankiplagat81@gmail.com>
2019-10-06 13:16:49 -0700

    created model for product, oroduct image and product tag.

--- booktime/booktime/settings.py
+++ booktime/booktime/settings.py
@@ -128,13 +128,13 @@ MEDIA_URL = '/media/'
 
 #email configuration settings.
 
-if not DEBUG:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
-    EMAIL_HOST_USER = "username"
-    EMAIL_HOST = 'smtp.domain.com' 
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True 
-    EMAIL_HOST_PASSWORD = "password"
+# if not DEBUG:
+#     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
+#     EMAIL_HOST_USER = "username"
+#     EMAIL_HOST = 'smtp.domain.com' 
+#     EMAIL_PORT = 587
+#     EMAIL_USE_TLS = True 
+#     EMAIL_HOST_PASSWORD = "password"
 
-else:
-    EMAIL_BACKEND = ("django.core.mail.backends.console.EmailBackend" )
+# else:
+EMAIL_BACKEND = ("django.core.mail.backends.console.EmailBackend" )
